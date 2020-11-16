fb53b1960f92488d2d79fbcc8a92ceab05bf4688
Nick Hennessy <njhnny@gmail.com>
2020-11-16 15:40:28 -0800

    add google api
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Loader } from "@googlemaps/js-api-loader";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+    const lat = $('#latitude').val();
+    const lon = $('#longitude').val();
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0b1f0c4ad33ac4bb014575429627407c`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=0b1f0c4ad33ac4bb014575429627407c`;
+    const loader = new Loader({
+      apiKey: "AIzaSyBOvgbJ-lq1v4Ub6XMvEkhamJoCvyIhX9cY",
+      version: "weekly",
+    });
+    
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -22,8 +29,16 @@ $(document).ready(function() {
     request.send();
 
     function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showHumidity').text(`The humidity in ${lat} ${lon} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
     }
+    
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 35.23, lng: -80.84 },
+        zoom: 8,
+      });
+    });
   });
+  
 });
