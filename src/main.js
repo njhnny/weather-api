1758c7a8788e2fd5c8ccd15cc232e41b70840a1d
Nick Hennessy <njhnny@gmail.com>
2020-11-16 17:03:27 -0800

    connect to google api

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Loader } from "@googlemaps/js-api-loader";
+//import { Loader } from "@googlemaps/js-api-loader";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
@@ -12,10 +12,10 @@ $(document).ready(function() {
 
     let request = new XMLHttpRequest();
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=0b1f0c4ad33ac4bb014575429627407c`;
-    const loader = new Loader({
-      apiKey: "AIzaSyBOvgbJ-lq1v4Ub6XMvEkhamJoCvyIhX9cY",
-      version: "weekly",
-    });
+    // const loader = new Loader({
+    //   apiKey: "AIzaSyBOvgbJ-lq1v4Ub6XMvEkhamJoCvyIhX9cY",
+    //   version: "weekly",
+    // });
     
 
     request.onreadystatechange = function() {
@@ -32,13 +32,22 @@ $(document).ready(function() {
       $('.showHumidity').text(`The humidity in ${lat} ${lon} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
     }
+
+    let map;
+
+    function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
     
-    loader.load().then(() => {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 35.23, lng: -80.84 },
-        zoom: 8,
-      });
-    });
+    // loader.load().then(() => {
+    //   map = new google.maps.Map(document.getElementById("map"), {
+    //     center: { lat: 35.23, lng: -80.84 },
+    //     zoom: 8,
+    //   });
+    // });
   });
   
 });
